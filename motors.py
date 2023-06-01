1ded694cea55a0167e17474696896a6fdaf86953
Nicolas Schmid <schmid.nicolas@hotmail.com>
2023-06-01 10:49:12 +0200

    improve stuff

--- motors.py
+++ motors.py
@@ -13,20 +13,22 @@ class Motors:
     def move_to_internal(self, alpha, beta, gamma, callback):
         try:
             print("Moving alpha")
-            self.move_one_angle("a", alpha)
+            self.move_one_angle("A", alpha)
             print("Moving beta")
-            self.move_one_angle("b", beta)
+            self.move_one_angle("<b", beta)
             print("Moving gamma")
-            self.move_one_angle("c", gamma)
+            self.move_one_angle("<v", gamma)
             callback()
         except Exception as e:
             print(e)
 
     def move_one_angle(self, code, angle):
         delta = np.rad2deg(angle[1]) - np.rad2deg(angle[0])
+        speed = int(np.round(np.abs(np.rad2deg(angle[2]) / delta)))
+        speed = 5
         command = "step m" + code + ".d" + ("R" if (delta < 0) else "L") + ".v" + str(
-            int(np.round(np.abs(np.rad2deg(angle[2]) / delta)))) + ".w" + str(
-            int(np.round(np.abs(delta)))) + ".cE"
+            speed) + ".w" + str(
+            int(np.round(np.abs(delta) * 100))) + ".cE"
         self.serial.write(bytes(command, 'ascii'))
         print("Command is")
         print(command)
