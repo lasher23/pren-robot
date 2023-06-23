c1fc9e5cedba5d569ec7444d6daf712a7010f083
Nicolas Schmid <schmid.nicolas@hotmail.com>
2023-06-23 16:55:16 +0200

    improve stuff

--- dummy.py
+++ dummy.py
@@ -1,3 +1,10 @@
 import numpy as np
+import serial
 
 print(str(np.arctan2(-45, -36)))
+
+ser = serial.Serial('/dev/ttyACM0', 57600, timeout=10000)
+command = "gpio pV.sH\n"
+ser.write(bytes(command, 'ascii'))
+command = "gpio pV.sL\n"
+ser.write(bytes(command, 'ascii'))
