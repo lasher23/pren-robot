091831c1ee0379cfcda58adef0c3f8a8257cdf04
Nicolas Schmid <nicolas.schmid@bison-group.com>
2023-06-30 12:38:47 +0200

    improve stuff

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ STOPPED = "STOPPED"
 MOVE_STEP_DOWN = "MOVE_STEP_DOWN"
 CHECK_FURTHER_DOWN = "CHECK_FURTHER_DOWN"
 INIT_DONE = "INIT_DONE"
-CIGARETTES_DROP = {"x": 160, "y": 190, "z": 4}
+CIGARETTES_DROP = {"x": 150, "y": 180, "z": 4}
 PET_DROP = {"x": 80, "y": 190, "z": 4}
-KRONKORKEN_DROP = {"x": -80, "y": 190, "z": 4}
-VALUABLE_DROP = {"x": -160, "y": 190, "z": 4}
+KRONKORKEN_DROP = {"x": -80, "y": 180, "z": 4}
+VALUABLE_DROP = {"x": -150, "y": 180, "z": 4}
 
 CAMERA_OFFSET = 60
 
