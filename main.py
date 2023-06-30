aac6e4daaba4fe10d3f6ed626ab9bfd241909fdc
Nicolas Schmid <nicolas.schmid@bison-group.com>
2023-06-30 11:57:22 +0200

    improve stuff

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ STOPPED = "STOPPED"
 MOVE_STEP_DOWN = "MOVE_STEP_DOWN"
 CHECK_FURTHER_DOWN = "CHECK_FURTHER_DOWN"
 INIT_DONE = "INIT_DONE"
-CIGARETTES_DROP = {"x": 160, "y": 150, "z": 20}
+CIGARETTES_DROP = {"x": 160, "y": 170, "z": 20}
 PET_DROP = {"x": 80, "y": 150, "z": 20}
-KRONKORKEN_DROP = {"x": -80, "y": 150, "z": 20}
-VALUABLE_DROP = {"x": -160, "y": 150, "z": 20}
+KRONKORKEN_DROP = {"x": -80, "y": 170, "z": 20}
+VALUABLE_DROP = {"x": -160, "y": 170, "z": 20}
 
-CAMERA_OFFSET = 57
+CAMERA_OFFSET = 60
 
 
 def calculate_target_coordinates_from_pixels(current_robot_position, on_camera_position_pixels, current_angles):
