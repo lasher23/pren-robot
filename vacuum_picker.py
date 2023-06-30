498deed3b49b8f96fcc3127bb9f260c6fdeff9cc
Nicolas Schmid <nicolas.schmid@bison-group.com>
2023-06-30 10:48:03 +0200

    improve stuff

--- vacuum_picker.py
+++ vacuum_picker.py
@@ -3,6 +3,15 @@ class VacuumPicker:
         self.serial = serial
 
     def pick_up(self):
+        print("calling vacuum picker to pick up")
+        command = "gpio pP.sH\n"
+        self.serial.write(bytes(command, 'ascii'))
+        while True:
+            result = self.serial.readline().decode().strip()
+            if "successfully" in result:
+                print("result is:")
+                print(result)
+                break
         command = "gpio pV.sH\n"
         self.serial.write(bytes(command, 'ascii'))
         while True:
@@ -13,7 +22,16 @@ class VacuumPicker:
                 break
 
     def drop_down(self):
+        print("calling vacuum picker to drop down")
         command = "gpio pV.sL\n"
+        self.serial.write(bytes(command, 'ascii'))
+        while True:
+            result = self.serial.readline().decode().strip()
+            if "successfully" in result:
+                print("result is:")
+                print(result)
+                break
+        command = "gpio pP.sL\n"
         self.serial.write(bytes(command, 'ascii'))
         while True:
             result = self.serial.readline().decode().strip()
